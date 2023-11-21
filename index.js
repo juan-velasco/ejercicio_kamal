7542e185f111225ba8a7b19563606bc4247029e7
Juan Velasco <jvelasco@iti.es>
2023-11-21 22:15:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World 2!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/health-check',
+        handler: (request, h) => {
+
+            return 'Healthy';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
