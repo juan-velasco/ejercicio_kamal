0e67fe3d934e21f5c88a5e86c7cf5ec1f71186d3
Juan Velasco <jvelasco@iti.es>
2023-11-28 09:29:32 +0100

    New version

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const init = async () => {
         path: '/',
         handler: (request, h) => {
 
-            return 'Hello World!';
+            return 'Hello World from new version!';
         }
     });
 
